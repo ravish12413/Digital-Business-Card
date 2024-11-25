05838d907d3ea56bce8ba61b1320baacd5595f3b
aad99bxp <aad99bxp@gmail.com>
2024-11-26 00:17:54 +0530

    CSS File and minor changes in app.js

--- public/app.js
+++ public/app.js
@@ -1,14 +1,16 @@
-// import firebaseConfig from './firebaseConfig.js';
+
 // Firebase configuration object (use your configuration details here)
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+
 
 firebase.initializeApp(firebaseConfig);
 
@@ -304,7 +306,7 @@ let uid, username;
     function injectCSS() {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
-      link.href = './css/user-card.css';  
+      link.href = './user-card.css';  
       document.head.appendChild(link);
 
       const faLink = document.createElement('link');
